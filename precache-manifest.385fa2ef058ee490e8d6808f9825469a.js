16e79339efb4731ab5e7b292323d381d7f4f700c
xiaofei <xiaofei@botpy.com>
2020-02-16 00:35:07 +0800

    3

---
+++ precache-manifest.385fa2ef058ee490e8d6808f9825469a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27ac5db152ac7eae8e30b19c78a89a06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38b1c75c6b8b1a127b40",
+    "url": "/static/css/2.97640232.chunk.css"
+  },
+  {
+    "revision": "38b1c75c6b8b1a127b40",
+    "url": "/static/js/2.20ec7f17.chunk.js"
+  },
+  {
+    "revision": "c30cff631386de87339e",
+    "url": "/static/js/main.3850db7c.chunk.js"
+  },
+  {
+    "revision": "542b4d221ecb2b0b89bd",
+    "url": "/static/js/runtime-main.a0b25e97.js"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/static/media/element-icons.d2f69a92.woff"
+  }
+]);
